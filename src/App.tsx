aa8074378135bf92d78c7fdaa7817933954f6426
kaliganoff <kaliganoff@yahoo.com>
2024-07-01 14:05:36 +0300

    feat: Implement start screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import StartScreen from "./components/StartScreen";
 
 function App() {
+  const [show, setShow] = useState(true);
 
-  return (
-    <>
-    </>
-  );
+  useEffect(() => {
+    const timeId = setTimeout(() => {
+      setShow(false);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeId);
+    };
+  }, []);
+
+  if (!show) {
+    return null;
+  }
+
+  return <StartScreen />;
 }
 
 export default App;
---
+++ src/components/StartScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./StartScreen.css";
+
+function StartScreen() {
+  return (
+    <div className="start-screen">
+      <img className="logo" src="logo.png" alt="" />
+    </div>
+  );
+}
+
+export default StartScreen;
